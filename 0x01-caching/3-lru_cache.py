0fd4787ad7e3d122590dd28ad3b7b2aefe0d8211
Kay Cee <se.kaycee@gmail.com>
2023-07-25 11:00:44 +0100

    feat: add lru cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+''' least recently used caching module. '''
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    ''' represent an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal mechanism when the limit is reached.
+    '''
+    def __init__(self):
+        ''' initialize the cache. '''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        ''' add an item in the cache. '''
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print('DISCARD:', lru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' retrieve an item by key. '''
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
